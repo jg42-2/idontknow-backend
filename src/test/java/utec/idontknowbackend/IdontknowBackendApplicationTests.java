5e50918390eeb9f19f5d6fb61b3e2aa00864b5df
jg <joseph.geraldo@utec.edu.pe>
2026-09-19 23:47:12 -0500

    mano tengo sueño

---
+++ src/test/java/utec/idontknowbackend/IdontknowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package utec.idontknowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdontknowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
